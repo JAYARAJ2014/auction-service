550dd580f6d3fb4f97f89807dcb2e4c2a41ef2ed
Jayaraj <jayaraj.sivadasan@gmail.com>
2021-10-09 18:14:25 -0400

    Added uuid and persistance logic for dynamodb

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,13 +1,24 @@
+import {v4 as uuid} from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
 
 // returns title as a key
     const {title} = JSON.parse(event.body);
     const now = new Date();
     const auction = {
+        id: uuid(),
         title, //if your key and value are same just one is enough. title:tile
         status:'OPEN',
         createdAt:now.toISOString(),
     };
+    await dynamodb.put({
+        TableName:'AuctionsTable',
+        Item:auction,
+    }).promise();
+
     return {
         statusCode: 201,
         body: JSON.stringify(auction),
