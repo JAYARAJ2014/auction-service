eac75d19391f4bcb9db6f2c3c6cab0d1bc0956ac
Jayaraj <jayaraj.sivadasan@gmail.com>
2021-10-10 16:06:20 -0400

    Update IAM actions and processauctions JS

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,9 @@
+import getEndedAuctions from './lib/getEndedAuctions';
+
 async function processAuctions(event, context) {
     console.log('Proessing auctions');
+    const autctionsToClose = await getEndedAuctions();
+    console.log(autctionsToClose);
 }
 
 export const handler = processAuctions;
