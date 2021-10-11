8ec7b7e2f19d1577a9d38554c7c5c1325f0b3b5a
Jayaraj <jayaraj.sivadasan@gmail.com>
2021-10-10 20:52:49 -0400

    Json schema validation for createAuction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -2,7 +2,8 @@ import {v4 as uuid} from 'uuid';
 import AWS from 'aws-sdk';
 import commonMiddleware from './lib/commonMiddleware';
 import createError from 'http-errors';
-
+import validator from '@middy/validator';
+import schema from './lib/schemas/createAuctionSchema'; 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
@@ -38,4 +39,5 @@ async function createAuction(event, context) {
     };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+.use(validator({inputSchema: createAuctionSchema}));
